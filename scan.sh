304f3bf0cf9acecc7742c1400a884d37247f9b70
Atul Abhishek <abhishekat@vmware.com>
2022-02-08 08:33:53 +0530

    add checkout

--- scan.sh
+++ scan.sh
@@ -12,10 +12,10 @@ TARGET_VERSION=$1
 # Will be empty string on not installed
 #installed_version=$(trivy --version)
 
-if [[ "$installed_version" != '' && "$installed_version" == *"$TARGET_VERSION"* ]]; then
-  echo "Already installed; skipping"
-  exit 0
-fi
+# if [[ "$installed_version" != '' && "$installed_version" == *"$TARGET_VERSION"* ]]; then
+#   echo "Already installed; skipping"
+#   exit 0
+# fi
 
 curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/master/contrib/install.sh | sudo sh -s -- -b /usr/local/bin
 
